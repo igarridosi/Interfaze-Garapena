ac4ab6e280db531ac19ff7c5185f9a612ccb3cfa
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-15 13:15:04 +0200

    6.ariketa egina

---
+++ WpfApp6/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (sender == TextBox1)
+                {
+                    TextBox2.Text = TextBox1.Text;
+                    TextBox1.Clear();
+                }
+                else if (sender == TextBox2)
+                {
+                    TextBox3.Text = TextBox2.Text;
+                    TextBox2.Clear();
+                }
+                else if (sender == TextBox3)
+                {
+                    TextBox1.Text = TextBox3.Text;
+                    TextBox3.Clear();
+                }
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Clear();
+            TextBox2.Clear();
+            TextBox3.Clear();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
