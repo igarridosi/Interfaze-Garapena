225a52b5d749c756d68c4e7e50a9b2e51b5b42cd
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-10-01 12:38:28 +0200

    3.ARIKETA-ko 11.Ariketa ila bukatua

---
+++ 3.ARIKETAK/Ariketa11/Ariketa11/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ariketa11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string data1 = nombre.Text;
+            string data2 = apellido1.Text;
+            string data3 = apellido2.Text;
+            string data4 = dni.Text;
+
+            string[] savedData = [data1, data2, data3, data4];
+
+            WelcomeWindow welcomeScreen = new WelcomeWindow(savedData);
+
+            welcomeScreen.Show();
+            this.Close();
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ 3.ARIKETAK/Ariketa11/Ariketa11/WelcomeWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ariketa11
+{
+    /// <summary>
+    /// Interaction logic for WelcomeWindow.xaml
+    /// </summary>
+    public partial class WelcomeWindow : Window
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        public WelcomeWindow(string[] userData)
+        {
+            InitializeComponent();
+
+            ShowData(userData);
+        }
+
+        public void ShowData(string[] userData)
+        {
+            if (userData == null || userData.Length < 4)
+            {
+                nombreWW.Text = "ERROR: Faltan datos";
+                return;
+            }
+
+            nombreWW.Text = "Nombre: " + userData[0];
+            apellido1WW.Text = "1º Apellido: " + userData[1];
+            apellido2WW.Text = "2º Apellido: " + userData[2];
+            dniWW.Text = "DNI: " + userData[3];
+        }
+
+    }
+}
